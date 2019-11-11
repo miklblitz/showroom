d79e92cb9f1027c9e7f80fd7e828a49bbb922cf2
Михман <miklblitz@yandex.ru>
2019-11-11 12:10:58 +0400

    sign-up has been returned to the sign page

--- src/components/sign/sign.component.jsx
+++ src/components/sign/sign.component.jsx
@@ -1,10 +1,12 @@
  import React from 'react';
- import SignIn from '../sign-in/sign-in.component.jsx';
+import SignIn from '../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up.component';
  import './sign.style.scss';
 
  const Sign = () =>(
-  <div clas='sign-in-and-sign-up'>
+   <div class='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
  )
 
